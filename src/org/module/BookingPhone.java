ff3256a5005d64941a581d713a5a169319e8a252
KingKalai <kalairayagiri@gmail.com>
2022-07-04 10:32:29 +0530

    Poco F4

--- src/org/module/BookingPhone.java
+++ src/org/module/BookingPhone.java
@@ -16,15 +16,18 @@ import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BookingPhone {
-	public static void main(String[] args) throws WebDriverException, IOException {
+	public static void main(String[] args) throws WebDriverException, IOException, InterruptedException {
 		WebDriver driver = null;
 		try {
 			System.setProperty("webdriver.chrome.driver", "D:\\program\\Flipkartphone\\driver\\chromedriver.exe");
 			driver = new ChromeDriver();
 			driver.get("https://www.flipkart.com/");
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
-			driver.findElement(By.xpath("//button[@class='_2KpZ6l _2doB4z']")).click();
 			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.findElement(By.xpath("(//input[@autocomplete='off'])[2]")).sendKeys("9578394612");
+			driver.findElement(By.xpath("(//input[@autocomplete='off'])[3]")).sendKeys("Kalai035$");
+			driver.findElement(By.xpath("(//span[starts-with(text(),'Login')])[2]")).click();
+			Thread.sleep(3000);
 			driver.findElement(By.xpath("//div[text()='Mobiles']")).click();
 			driver.findElement(By.xpath("(//div[@class='_HYyiu'])[2]")).click();
 			driver.findElement(By.xpath("//div[text()='POCO F4 5G (Night Black, 128 GB)']")).click();
@@ -32,7 +35,7 @@ public class BookingPhone {
 			List<String> l = new ArrayList<String>(s);
 			driver.switchTo().window(l.get(1));
 			driver.findElement(By.cssSelector("svg._1KOMV2[width='16']")).click();
-			driver.findElement(By.xpath("//span[text()='Continue']")).click();
+
 		} finally {
 			TakesScreenshot ts = (TakesScreenshot) driver;
 			FileUtils.copyFile(ts.getScreenshotAs(OutputType.FILE),
